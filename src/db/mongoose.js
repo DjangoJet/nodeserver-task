caf248c22d01d64e288e994df177562952f51d80
mike <mike@email.com>
2019-03-11 01:28:55 +0100

    add login user endpoint and add validation unique to email

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,3 +1,7 @@
 const mongoose = require('mongoose')
 
-mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TaskApp')
+mongoose.connect(process.env.MONGODB_URI || 'mongodb://localhost:27017/TaskApp', {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+})
