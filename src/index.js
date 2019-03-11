d292a895c2e25ef91a6d38b0b6f70e9395f8b83c
mike <mike@email.com>
2019-03-11 03:27:24 +0100

    add tokens to user model and create tokens to login or create user

--- src/index.js
+++ src/index.js
@@ -15,17 +15,13 @@ app.listen(port, () => {
     console.log('Server is up on port ' + port)
 })
 
-const bcrypt = require('bcryptjs')
+const jwt = require('jsonwebtoken')
 
 const myFunction = async () => {
-    const password = 'Monitor1234'
-    const hashedPassword = await bcrypt.hash(password, 4)
-
-    console.log(password)
-    console.log(hashedPassword)
-
-    const isMatch = await bcrypt.compare('Monitor1234', hashedPassword)
-    console.log(isMatch)
+    const token = jwt.sign({ _id: 'abc123' }, 'thisismynewcourse', { expiresIn: '7 days'})
+    console.log(token)
+    const data = jwt.verify(token, 'thisismynewcourse')
+    console.log(data)
 }
 
 myFunction()
